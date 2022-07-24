b0cdb309c592bfa965c5d308b98a8bac0792d034
Peter <peterjacobs717@gmail.com>
2022-07-24 11:34:37 +0200

    new

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'; //imports react. can now use react features
+
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')); //main hook
+root.render(<App />); //this is jsx -> special syntax invetnted by javascript team
